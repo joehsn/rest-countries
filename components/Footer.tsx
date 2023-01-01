108526adb952cc37c9bd81a8c063eb1303ae33b7
Youssef Hassan <89865074+joehsn@users.noreply.github.com>
2023-01-01 16:19:21 +0200

    footer created

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center px-4 py-4 text-white bg-slate-900">
+      <span>
+        Coded with ❤️ by{" "}
+        <a
+          className="text-teal-400 hover:text-teal-300"
+          href="https://linktr.ee/joehsn"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @joehsn
+        </a>
+        .
+      </span>
+    </footer>
+  );
+}
