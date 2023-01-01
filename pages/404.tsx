a0e8e7969a745b5b26c4f36ce4a0a36a7c394007
Youssef Hassan <89865074+joehsn@users.noreply.github.com>
2023-01-01 16:25:32 +0200

    not done yet

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function NotFound({}: Props) {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-300 dark:bg-dark-blue-dm">
+      <h1 className="text-lg lg:text-3xl">404</h1>
+    </div>
+  );
+}
