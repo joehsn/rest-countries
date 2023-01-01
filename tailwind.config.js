a6f681a8a4379291ea0ff5a92ec7247e3448fb3b
Youssef Hassan <89865074+joehsn@users.noreply.github.com>
2023-01-01 16:27:01 +0200

    back link shadow added

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
       fontFamily: {
         nunito: ["Nunito Sans", "sans-serif"],
       },
+      boxShadow: {
+        "back-link":
+          "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.12) 0px 2px 16px 0px",
+      },
     },
   },
   plugins: [],
