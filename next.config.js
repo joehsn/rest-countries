6be41f023ee0a227d792695be616dab1b931263a
Youssef Hassan <89865074+joehsn@users.noreply.github.com>
2023-01-01 16:24:31 +0200

    image path changed to SVG instead of PNG

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "flagcdn.com",
-        pathname: "/*/**",
+        pathname: "/*",
       },
       {
         protocol: "https",
